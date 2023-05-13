ccf5fdb2b9a4cee9b2f447b3c56105336fd9fb65
davidngo123 <ngodavid215@gmail.com>
2023-05-13 06:24:47 -0700

    didn't push newest lcient

--- JavaClient/Client.java
+++ JavaClient/Client.java
@@ -2,23 +2,10 @@ import java.io.*;
 import java.net.*;
 import java.net.http.*;
 import javax.xml.parsers.*;
-import javax.xml.xpath.*;
 import org.w3c.dom.*;
-
-import org.w3c.dom.Node;
-
-
-import java.io.*;
-import java.net.*;
-import java.net.http.*;
-import javax.xml.parsers.*;
-import javax.xml.xpath.*;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.*;
 import javax.xml.transform.stream.*;
-import org.w3c.dom.*;
-
-import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
 
 /**
@@ -27,8 +14,12 @@ import org.xml.sax.InputSource;
  */
 public class Client {
     private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    private static String url;
 
     public static void main(String... args) throws Exception {
+        String port = args[1];
+        String host = args[0];
+        url = "http://" + host + ":" + port;
         System.out.println(add() == 0);
         System.out.println(add(1, 2, 3, 4, 5) == 15);
         System.out.println(add(2, 4) == 6);
@@ -42,84 +33,81 @@ public class Client {
     public static int add(int lhs, int rhs) throws Exception {
         String xml = buildXml("add", lhs, rhs);
         String body = send(xml);
-        String answer = readBody(body);
-
-        return parseInt(answer);
+        return readBody(body);
     }
     public static int add(Integer... params) throws Exception {
         String xml = buildXml("add", (Object[]) params);
         String body = send(xml);
-        String answer = readBody(body);
-
-        return parseInt(answer);
+        return readBody(body);
     }
     public static int subtract(int lhs, int rhs) throws Exception {
         String xml = buildXml("subtract", lhs, rhs);
         String body = send(xml);
-        String answer = readBody(body);
-
-        return parseInt(answer);
+        return readBody(body);
     }
     public static int multiply(int lhs, int rhs) throws Exception {
         String xml = buildXml("multiply", lhs, rhs);
         String body = send(xml);
-        String answer = readBody(body);
-        return parseInt(answer);
+        return readBody(body);
     }
     public static int multiply(Integer... params) throws Exception {
         String xml = buildXml("multiply", (Object[]) params);
         String body = send(xml);
-        String answer = readBody(body);
-        return parseInt(answer);
+        return readBody(body);
     }
     public static int divide(int lhs, int rhs) throws Exception {
         String xml = buildXml("divide", lhs, rhs);
         String body = send(xml);
-        String answer = readBody(body);
-        return parseInt(answer);
+        return readBody(body);
     }
     public static int modulo(int lhs, int rhs) throws Exception {
+        String xml = buildXml("modulo", lhs, rhs);
         String body = send(xml);
-        String answer = readBody(body);
-        return parseInt(answer);
+        return readBody(body);
     }
 
 
     private static String send(String xml){
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://" + host + ":" + port))
-                .POST(HttpRequest.BodyPublishers.ofString(xml))
-                .header("Content-Type", "text/xml")
-                .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        int statusCode = response.statusCode();
-        String body = response.body();
+        String body = "";
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .POST(HttpRequest.BodyPublishers.ofString(xml))
+                    .header("Content-Type", "text/xml")
+                    .build();
 
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            body = response.body();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
         return body;
     }
 
-    private static String readBody(String xml) throws Exception {
+    private static int readBody(String xml) throws Exception {
         DocumentBuilder db = dbf.newDocumentBuilder();
         InputSource is = new InputSource();
-        is.setCharacterStream(new StringReader(xmlRecords));
+        is.setCharacterStream(new StringReader(xml));
         Document doc = db.parse(is);
 
 
         NodeList error = doc.getElementsByTagName("fault");
 
-        if (faultNode.getLength() != 0) {
+        if (error.getLength() != 0) {
             NodeList i4 = doc.getElementsByTagName("i4");
             NodeList errorString = doc.getElementsByTagName("string");
-            String errorCode3 = i4.item(0).getTextContent().trim();
-            String faultString = errorCode3.item(0).getTextContent().trim();
+            String errorCode3 = i4.item(0).getTextContent();
+            String str = errorString .item(0).getTextContent();
 
-            throw new Exception("Error Code: " + code + ", Fault: " + str);
+            throw new Exception("Error Code: " + errorCode3.trim() + ", Fault: " + str.trim());
         }
 
         NodeList value = doc.getElementsByTagName("value");
-        return value.item(0).getTextContent().trim();
+        String answer = value.item(0).getTextContent().trim();
+        return Integer.parseInt(answer);
     }
 
     private static String buildXml(String methodName, Object... args) throws Exception {
